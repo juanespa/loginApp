b5baad56cfdf769e534552e0c9bd2ca0b9b11f6d
Juan Felipe España Arias <juanfel_777@hotmail.com>
2018-04-03 10:11:36 -0500

    Base de datos
    se instalo firebase y angularfire2 para la base de datos NoSQL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDW3ToNz7J5mjXK2a0R8-5kdCRG5YNAmv0',
+    authDomain: 'loginapp-7f7bb.firebaseapp.com',
+    databaseURL: 'https://loginapp-7f7bb.firebaseio.com',
+    projectId: 'loginapp-7f7bb',
+    storageBucket: 'loginapp-7f7bb.appspot.com',
+    messagingSenderId: '411028075278'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDW3ToNz7J5mjXK2a0R8-5kdCRG5YNAmv0',
+    authDomain: 'loginapp-7f7bb.firebaseapp.com',
+    databaseURL: 'https://loginapp-7f7bb.firebaseio.com',
+    projectId: 'loginapp-7f7bb',
+    storageBucket: 'loginapp-7f7bb.appspot.com',
+    messagingSenderId: '411028075278'
+  }
 };
